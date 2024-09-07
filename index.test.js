366b9a01a9225b4481c0ff4f3b99eb9b186399ad
Stephen Yeargin <stephen@yearg.in>
2024-09-07 11:44:31 -0500

    Fix tests

--- index.test.js
+++ index.test.js
@@ -2,6 +2,16 @@ const fs = require('node:fs');
 const { render } = require('./index');
 
 describe('render', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+    const mockDate = new Date(2024, 6, 1);
+    jest.setSystemTime(mockDate);
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   it('should render', async () => {
     const jsonResume = fs.readFileSync('./tests/fixtures/test-resume.json');
     const resume = JSON.parse(jsonResume);
